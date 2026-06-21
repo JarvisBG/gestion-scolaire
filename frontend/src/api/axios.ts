dd84a84693415a878a35f18cfd5c3431f2b785fb
JarvisBG <jarvismboummeu28@gmail.com>
2026-06-21 06:33:28 +0100

    Mise a jour de baseURL vers l'API en production

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // L'URL de ton backend FastAPI
+  baseURL: 'https://gestion-scolaire-api.vercel.app/',
+  // baseURL: 'http://127.0.0.1:8000', // L'URL de ton backend FastAPI
 });
 
 // Ajoute automatiquement le token si tu as géré l'authentification
